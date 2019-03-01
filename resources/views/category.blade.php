d6ba3e3986b93778ac321c8e3f88c2207b3bdfd1
provinciateramo <g.dicarlo@provincia.teramo.it>
2019-03-02 00:33:16 +0100

    Card Bootstrap Italia

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -10,15 +10,11 @@
     {!! get_search_form(false) !!}
   @endif
   <div class="row mt-4">
-    <div class="card-columns ">
   @while (have_posts()) @php the_post() @endphp
-    <!--<div class="col col-sm-6 p-4">
-      <div class="border-top border-primary border-top-thick">-->
+    <div class="col col-sm-6 p-4">
       @include('partials.content')
-      <!--</div>
-    </div>-->
+    </div>
   @endwhile
     </div>
-  </div>
   {!! get_the_posts_navigation() !!}
 @endsection
