3ba56aea03ca7c8a3ce8134f8b1b42d46528c984
Gianluca <g.dicarlo@provincia.teramo.it>
2019-03-02 01:01:31 +0100

    Fix path font bootstrap italia

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <script>window.__PUBLIC_PATH__ = "{{ \App\asset_path('bootstrap-italia/fonts') }}"</script>
   @php wp_head() @endphp
 </head>
