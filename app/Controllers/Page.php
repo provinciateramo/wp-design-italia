481fa6d4ddf5a89b6cc2b081c294bf4b7ead0590
provinciateramo <g.dicarlo@provincia.teramo.it>
2019-09-24 22:20:32 +0200

    Aggiunto il sommario e ottimizzata la visualizzazione di pagine e post

---
+++ app/Controllers/Page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: gianluca
+ * Date: 17/01/19
+ * Time: 21.59
+ *
+ * @package Controllers\App
+ */
+
+namespace app\Controllers;
+
+use Sober\Controller\Controller;
+use app\Controllers\Single;
+
+/**
+ * Class Page
+ *
+ * @package app\Controllers
+ */
+class Page extends Controller {
+
+	/**
+	 * Declare implementing tree.
+	 *
+	 * @var bool
+	 */
+	protected $tree = true;
+
+
+}
