652c1e9c3ab6f299d16e3a45a70d08672650bdcf
provinciateramo <g.dicarlo@provincia.teramo.it>
2019-10-14 15:41:24 +0200

    Corretto il problema nella distribuzione dei file di bootstrap italia

--- resources/assets/build/webpack.config.js
+++ resources/assets/build/webpack.config.js
@@ -19,178 +19,182 @@ let webpackConfig = {
   entry: config.entry,
   devtool: (config.enabled.sourceMaps ? '#source-map' : undefined),
   output: {
-    path: config.paths.dist,
-    publicPath: config.publicPath,
-    filename: `scripts/${assetsFilenames}.js`,
+	path: config.paths.dist,
+	publicPath: config.publicPath,
+	filename: `scripts/${assetsFilenames}.js`,
   },
   stats: {
-    hash: false,
-    version: false,
-    timings: false,
-    children: false,
-    errors: false,
-    errorDetails: false,
-    warnings: false,
-    chunks: false,
-    modules: false,
-    reasons: false,
-    source: false,
-    publicPath: false,
+	hash: false,
+	version: false,
+	timings: false,
+	children: false,
+	errors: false,
+	errorDetails: false,
+	warnings: false,
+	chunks: false,
+	modules: false,
+	reasons: false,
+	source: false,
+	publicPath: false,
   },
   module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        include: config.paths.assets,
-        use: 'eslint',
-      },
-      {
-        enforce: 'pre',
-        test: /\.(js|s?[ca]ss)$/,
-        include: config.paths.assets,
-        loader: 'import-glob',
-      },
-      {
-        test: /\.js$/,
-        exclude: [/node_modules(?![/|\\](bootstrap|foundation-sites))/],
-        use: [
-          { loader: 'cache' },
-          { loader: 'buble', options: { objectAssign: 'Object.assign' } },
-        ],
-      },
-      {
-        test: /\.css$/,
-        include: config.paths.assets,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style',
-          use: [
-            { loader: 'cache' },
-            { loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
-            {
-              loader: 'postcss', options: {
-                config: { path: __dirname, ctx: config },
-                sourceMap: config.enabled.sourceMaps,
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.scss$/,
-        include: config.paths.assets,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style',
-          use: [
-            { loader: 'cache' },
-            { loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
-            {
-              loader: 'postcss', options: {
-                config: { path: __dirname, ctx: config },
-                sourceMap: config.enabled.sourceMaps,
-              },
-            },
-            { loader: 'resolve-url', options: { sourceMap: config.enabled.sourceMaps } },
-            {
-              loader: 'sass', options: {
-                sourceMap: config.enabled.sourceMaps,
-                sourceComments: true,
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-        include: config.paths.assets,
-        loader: 'url',
-        options: {
-          limit: 4096,
-          name: `[path]${assetsFilenames}.[ext]`,
-        },
-      },
-      {
-        test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-        include: /node_modules/,
-        loader: 'url',
-        options: {
-          limit: 4096,
-          outputPath: 'vendor/',
-          name: `${config.cacheBusting}.[ext]`,
-        },
-      },
-    ],
+	rules: [
+	  {
+		enforce: 'pre',
+		test: /\.js$/,
+		include: config.paths.assets,
+		use: 'eslint',
+	  },
+	  {
+		enforce: 'pre',
+		test: /\.(js|s?[ca]ss)$/,
+		include: config.paths.assets,
+		loader: 'import-glob',
+	  },
+	  {
+		test: /\.js$/,
+		exclude: [/node_modules(?![/|\\](bootstrap|foundation-sites))/],
+		use: [
+		  { loader: 'cache' },
+		  { loader: 'buble', options: { objectAssign: 'Object.assign' } },
+		],
+	  },
+	  {
+		test: /\.css$/,
+		include: config.paths.assets,
+		use: ExtractTextPlugin.extract({
+		  fallback: 'style',
+		  use: [
+			{ loader: 'cache' },
+			{ loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
+			{
+			  loader: 'postcss', options: {
+				config: { path: __dirname, ctx: config },
+				sourceMap: config.enabled.sourceMaps,
+			  },
+			},
+		  ],
+		}),
+	  },
+	  {
+		test: /\.scss$/,
+		include: config.paths.assets,
+		use: ExtractTextPlugin.extract({
+		  fallback: 'style',
+		  use: [
+			{ loader: 'cache' },
+			{ loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
+			{
+			  loader: 'postcss', options: {
+				config: { path: __dirname, ctx: config },
+				sourceMap: config.enabled.sourceMaps,
+			  },
+			},
+			{ loader: 'resolve-url', options: { sourceMap: config.enabled.sourceMaps } },
+			{
+			  loader: 'sass', options: {
+				sourceMap: config.enabled.sourceMaps,
+				sourceComments: true,
+			  },
+			},
+		  ],
+		}),
+	  },
+	  {
+		test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
+		include: config.paths.assets,
+		loader: 'url',
+		options: {
+		  limit: 4096,
+		  name: `[path]${assetsFilenames}.[ext]`,
+		},
+	  },
+	  {
+		test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
+		include: /node_modules/,
+		loader: 'url',
+		options: {
+		  limit: 4096,
+		  outputPath: 'vendor/',
+		  name: `${config.cacheBusting}.[ext]`,
+		},
+	  },
+	],
   },
   resolve: {
-    modules: [
-      config.paths.assets,
-      'node_modules',
-    ],
-    enforceExtension: false,
+	modules: [
+	  config.paths.assets,
+	  'node_modules',
+	],
+	enforceExtension: false,
   },
   resolveLoader: {
-    moduleExtensions: ['-loader'],
+	moduleExtensions: ['-loader'],
   },
   externals: {
-    jquery: 'jQuery',
+	jquery: 'jQuery',
   },
   plugins: [
-    new CleanPlugin([config.paths.dist], {
-      root: config.paths.root,
-      verbose: false,
-    }),
-    /**
-     * It would be nice to switch to copy-webpack-plugin, but
-     * unfortunately it doesn't provide a reliable way of
-     * tracking the before/after file names
-     */
-    new CopyWebpackPlugin(
-      [{from: `${config.paths.root}/node_modules/bootstrap-italia/dist`, to:`${config.paths.assets}/bootstrap-italia`}],
-      { debug: 'debug' }
-    ),
-    new CopyWebpackPlugin(
-      [{from: `${config.paths.assets}/bootstrap-italia`, to:`${config.paths.dist}/bootstrap-italia`}],
-      { debug: 'debug' }
-    ),
-    new CopyGlobsPlugin({
-      pattern: config.copy,
-      output: `[path]${assetsFilenames}.[ext]`,
-      manifest: config.manifest,
-    }),
-    new ExtractTextPlugin({
-      filename: `styles/${assetsFilenames}.css`,
-      allChunks: true,
-      disable: (config.enabled.watcher),
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: 'popper.js/dist/umd/popper.js',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: config.enabled.optimize,
-      debug: config.enabled.watcher,
-      stats: { colors: true },
-    }),
-    new webpack.LoaderOptionsPlugin({
-      test: /\.s?css$/,
-      options: {
-        output: { path: config.paths.dist },
-        context: config.paths.assets,
-      },
-    }),
-    new webpack.LoaderOptionsPlugin({
-      test: /\.js$/,
-      options: {
-        eslint: { failOnWarning: false, failOnError: true },
-      },
-    }),
-    new StyleLintPlugin({
-      failOnError: !config.enabled.watcher,
-      syntax: 'scss',
-    }),
-    new FriendlyErrorsWebpackPlugin(),
+	new CleanPlugin([config.paths.dist], {
+	  root: config.paths.root,
+	  verbose: false,
+	}),
+	/**
+	 * It would be nice to switch to copy-webpack-plugin, but
+	 * unfortunately it doesn't provide a reliable way of
+	 * tracking the before/after file names
+	 */
+	/*new CopyWebpackPlugin(
+	  [{from: `${config.paths.root}/node_modules/bootstrap-italia/dist`, to:`${config.paths.assets}/bootstrap-italia`}],
+	  { debug: 'debug' }
+	),
+	new CopyWebpackPlugin(
+	  [{from: `${config.paths.assets}/bootstrap-italia`, to:`${config.paths.dist}/bootstrap-italia`}],
+	  { debug: 'debug' }
+	),*/
+	new CopyWebpackPlugin(
+		[{from: `${config.paths.root}/node_modules/bootstrap-italia/dist`, to:`${config.paths.dist}/bootstrap-italia`}],
+		{ debug: 'debug' }
+	),
+	new CopyGlobsPlugin({
+	  pattern: config.copy,
+	  output: `[path]${assetsFilenames}.[ext]`,
+	  manifest: config.manifest,
+	}),
+	new ExtractTextPlugin({
+	  filename: `styles/${assetsFilenames}.css`,
+	  allChunks: true,
+	  disable: (config.enabled.watcher),
+	}),
+	new webpack.ProvidePlugin({
+	  $: 'jquery',
+	  jQuery: 'jquery',
+	  'window.jQuery': 'jquery',
+	  Popper: 'popper.js/dist/umd/popper.js',
+	}),
+	new webpack.LoaderOptionsPlugin({
+	  minimize: config.enabled.optimize,
+	  debug: config.enabled.watcher,
+	  stats: { colors: true },
+	}),
+	new webpack.LoaderOptionsPlugin({
+	  test: /\.s?css$/,
+	  options: {
+		output: { path: config.paths.dist },
+		context: config.paths.assets,
+	  },
+	}),
+	new webpack.LoaderOptionsPlugin({
+	  test: /\.js$/,
+	  options: {
+		eslint: { failOnWarning: false, failOnError: true },
+	  },
+	}),
+	new StyleLintPlugin({
+	  failOnError: !config.enabled.watcher,
+	  syntax: 'scss',
+	}),
+	new FriendlyErrorsWebpackPlugin(),
   ],
 };
 
@@ -208,13 +212,13 @@ if (config.enabled.cacheBusting) {
   const WebpackAssetsManifest = require('webpack-assets-manifest');
 
   webpackConfig.plugins.push(
-    new WebpackAssetsManifest({
-      output: 'assets.json',
-      space: 2,
-      writeToDisk: false,
-      assets: config.manifest,
-      replacer: require('./util/assetManifestsFormatter'),
-    })
+	new WebpackAssetsManifest({
+	  output: 'assets.json',
+	  space: 2,
+	  writeToDisk: false,
+	  assets: config.manifest,
+	  replacer: require('./util/assetManifestsFormatter'),
+	})
   );
 }
 
